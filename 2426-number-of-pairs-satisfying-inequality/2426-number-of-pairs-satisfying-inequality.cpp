7b0eac9f0aa45f13efbea9f05d2f713ca09ab063
Vedang Dadape <81163049+Vedang12d@users.noreply.github.com>
2023-01-03 22:49:04 +0530

    Time: 586 ms (60.11%), Space: 130.2 MB (48.25%) - LeetHub

---
+++ 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.cpp
@@ -0,0 +1,16 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;  
+typedef tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+class Solution {
+public:
+    long long numberOfPairs(vector<int>& nums1, vector<int>& nums2, int diff) {
+        ordered_set st;
+        long long ans=0;
+        for(int i=0;i<nums1.size();i++){
+            ans+=st.order_of_key(nums1[i]-nums2[i]+diff+1);
+            st.insert(nums1[i]-nums2[i]);
+        }
+        return ans;
+    }
+};
