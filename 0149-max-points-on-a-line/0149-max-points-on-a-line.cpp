84d55ee80d5ca4f878f6cbb40bd48da85b95023b
Vedang Dadape <81163049+Vedang12d@users.noreply.github.com>
2023-01-09 10:40:09 +0530

    Time: 25 ms (97.05%), Space: 11.4 MB (75.05%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -5,10 +5,8 @@ public:
         int ans=1;
         for(int i=0;i<points.size();i++){
             for(int j=i+1;j<points.size();j++){
-                double a;
-                if(points[i][0]==points[j][0])
-                    a=DBL_MAX;
-                else
+                double a=DBL_MAX;
+                if(points[i][0]!=points[j][0])
                     a=(0.0+points[j][1]-points[i][1])/(0.0+points[j][0]-points[i][0]);
                 mp[a]++;
                 ans=max(ans,mp[a]+1);
