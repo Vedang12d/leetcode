8ecb4997ff702f5063646477100f69219d42aaff
Vedang Dadape <81163049+Vedang12d@users.noreply.github.com>
2023-01-09 11:51:36 +0530

    Time: 114 ms (80.04%), Space: 58.2 MB (76.50%) - LeetHub

---
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        unordered_map<int,int> freq,need;
+        for(auto num:nums)
+            freq[num]++;
+        for(auto num:nums){
+            if(!freq.count(num))
+                continue;
+            else if(need.count(num)){
+                if(--need[num]==0)
+                    need.erase(num);
+                need[num+1]++;
+            }
+            else if(freq.count(num+1)&&freq.count(num+2)){
+                if(--freq[num+1]==0)
+                    freq.erase(num+1);
+                if(--freq[num+2]==0)
+                    freq.erase(num+2);
+                need[num+3]++;
+            }
+            else
+                return 0;
+            if(--freq[num]==0)
+                freq.erase(num);
+        }
+        return 1;
+    }
+};
