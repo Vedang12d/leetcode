fb7de106d96e959b2236effd3135fbf342ae966f
Vedang Dadape <81163049+Vedang12d@users.noreply.github.com>
2023-02-07 18:35:59 +0530

    Added solution - LeetHub

---
+++ Length of the longest subarray with positive product - GFG/length-of-the-longest-subarray-with-positive-product.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+    public:
+        // Function to return the length of the
+        //longest subarray with ppositive product
+        int maxLength(vector<int> &arr,int n){
+            int plen=0,nlen=0,ans=0;
+            for(int i=0;i<n;i++){
+                if(!arr[i])
+                    plen=nlen=0;
+                else if(arr[i]>0){
+                    plen++;
+                    if(nlen)
+                        nlen++;
+                    ans=max(ans,plen);
+                }else{
+                    swap(plen,nlen);
+                    nlen++;
+                    if(plen)
+                        plen++;
+                    ans=max(ans,plen);
+                }
+            }
+            return ans;
+        }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    vector<int> arr(n);
+	    for(int i = 0;i<n;i++){
+	        cin>>arr[i];
+	    }
+	    Solution ob;
+	    int res = ob.maxLength(arr,n);
+	    cout<<res<<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
