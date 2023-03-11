deaa5ba184f86d99224f9cd8a716730b69298ac7
Vedang Dadape <81163049+Vedang12d@users.noreply.github.com>
2023-03-11 10:06:08 +0530

    Added solution - LeetHub

---
+++ Yet another query problem - GFG/yet-another-query-problem.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> solveQueries(int N, int num, vector<int> &A, vector<vector<int>> &Q) {
+        vector<int> freq(N);
+        unordered_map<int,int> mp;
+        for(int i=N-1;i>=0;i--)
+            freq[i]=++mp[A[i]];
+        vector<int> ans;
+        for(auto &q:Q){
+            int ct=0;
+            for(int i=q[0];i<=q[1];i++){
+                if(freq[i]==q[2])
+                    ct++;
+            }
+            ans.push_back(ct);
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int num;
+        cin>>num;
+        vector<int> A(N);
+        for(int i=0;i<N;i++){
+            cin>>A[i];
+        }
+        vector<vector<int>> Q(num, vector<int>(3));
+        for(int i=0;i<num;i++){
+            for(int j=0;j<3;j++){
+                cin>>Q[i][j];
+            }
+        }
+        Solution obj;
+        vector<int> res = obj.solveQueries(N, num, A, Q);
+        
+        for(auto ele:res){
+            cout<<ele<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+// } Driver Code Ends
