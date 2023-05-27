4d384f9e90fe4c158306936a547f8bfc5b0e5154
Vedang Dadape <81163049+Vedang12d@users.noreply.github.com>
2023-05-27 09:54:11 +0530

    Time: 280 ms (46.45%), Space: 132.1 MB (81.04%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     vector<int> dp;
-    int rec(int i, vector<int> &sv){
-        if(i>=sv.size())
-            return 0;
-        if(dp[i]!=INT_MIN)
-            return dp[i];
-        int sum=0,val=INT_MIN;
-        for(int j=i;j<i+3 and j<sv.size();j++){
-            sum+=sv[j];
-            dp[i]=max(dp[i],sum-rec(j+1,sv));
-        }
-        return dp[i];
-    }
     string stoneGameIII(vector<int>& stoneValue) {
-        dp.resize(stoneValue.size(),INT_MIN);
-        int val=rec(0,stoneValue);
-        return (val>0?"Alice":(val<0)?"Bob":"Tie");
+        int n=stoneValue.size();
+        vector<int> dp(n+1,INT_MIN);
+        dp[n]=0;
+        for(int i=n-1;i>=0;i--){
+            int sum=0;
+            for(int j=i;j<min(n,i+3);j++){
+                sum+=stoneValue[j];
+                dp[i]=max(dp[i],sum-dp[j+1]);
+            }
+        }
+        return (dp[0]>0?"Alice":(dp[0]<0)?"Bob":"Tie");
     }
 };
